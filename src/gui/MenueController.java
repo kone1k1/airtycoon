b6ec48fce54ac47c031d2c5e9d77ca697aa7c0bc
BuildTools <unconfigured@null.spigotmc.org>
2017-02-23 14:05:31 +0100

    abfrage der arraylänge durch feste variable entfernt

--- src/gui/MenueController.java
+++ src/gui/MenueController.java
@@ -23,7 +23,7 @@ import javafx.scene.control.TextField;
  */
 public class MenueController implements Initializable {
 
-    private xml_reader xml = new xml_reader();
+    private final xml_reader xml = new xml_reader();
 
     private Player[] playerlist;
 
@@ -56,14 +56,14 @@ public class MenueController implements Initializable {
     }
 
     private void loadPlayers() {
-        playerlist = new Player[xml.getNumber_players()];
-        System.arraycopy(xml.getPlayers(), 0, playerlist, 0, xml.getNumber_players());
+        playerlist = new Player[xml.getPlayers().length];
+        System.arraycopy(xml.getPlayers(), 0, playerlist, 0, xml.getPlayers().length);
         savegame = FXCollections.observableArrayList(playerlist);
         lstSaves.setItems(savegame);
     }
 
     @FXML
     private void exitProg() {
-        System.exit(1);
+        System.exit(0);
     }
 }
